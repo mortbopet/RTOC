53bdd389a513f3a1a055d849b0ef65b5969010fb
Morten Borup Petersen <morten_bp@live.dk>
2018-02-28 17:49:14 +0100

    Add OpenCV to CMakeLists and add example in main

--- src/CellSorter/main.cpp
+++ src/CellSorter/main.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
 
 #include "prettyprint.h"
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
 
-using namespace std;
+using namespace cv;
 
-int main()
-{
-    PP::header("CellSorter");
-    return 0;
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    printf("usage: DisplayImage.out <Image_Path>\n");
+    return -1;
+  }
+
+  Mat image;
+  image = imread(argv[1], 1);
+
+  if (!image.data) {
+    printf("No image data \n");
+    return -1;
+  }
+  namedWindow("Display Image", WINDOW_AUTOSIZE);
+  imshow("Display Image", image);
+
+  waitKey(0);
+
+  return 0;
 }
