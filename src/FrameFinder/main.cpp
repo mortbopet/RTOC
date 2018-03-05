0a0fddaee983d23605afdf205063d387be9633df
Morten Borup Petersen <morten_bp@live.dk>
2018-03-05 17:01:09 +0100

    Except images to be in fagprojekt/data/*

--- src/FrameFinder/main.cpp
+++ src/FrameFinder/main.cpp
@@ -8,18 +8,16 @@ using namespace std;
  * Set _PATH_DATA to the folder in which your images recide
  *
  */
-#define _PATH_DATA      "/Users/jl/Downloads/fagprojekt/data/ImgD1/"
-#define _PATH_ACCEPTED  "./accepted.txt"
+#define _PATH_DATA "../../data/ImgD1/"
+#define _PATH_ACCEPTED "./accepted.txt"
 #define _PATH_DISCARDED "./discarded.txt"
 
 int main() {
-
     vector<string> a;
 
     FF::get_files_sorted(a, _PATH_DATA, FF::FF_ONLY_FILE);
 
-    FF::accept_or_reject(a, _PATH_DATA, _PATH_ACCEPTED,_PATH_DISCARDED);
+    FF::accept_or_reject(a, _PATH_DATA, _PATH_ACCEPTED, _PATH_DISCARDED);
 
     return 0;
-
 }
