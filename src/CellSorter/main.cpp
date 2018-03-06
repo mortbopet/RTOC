380cebf73c1709a740d3508c6be0e6d958622cf1
Eskildbs <eskildbsorensen@hotmail.com>
2018-03-06 17:10:53 +0100

    work with local path

--- src/CellSorter/main.cpp
+++ src/CellSorter/main.cpp
@@ -7,6 +7,7 @@
 using namespace cv;
 
 int main(int argc, char **argv) {
+
   if (argc != 2) {
     printf("usage: DisplayImage.out <Image_Path>\n");
     return -1;
@@ -15,7 +16,8 @@ int main(int argc, char **argv) {
   RBC a;
 
   Mat image;
-  image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+  //image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    image = imread("/Users/eskidlbs/Desktop/data/ImgD1/081116D1_28932.png", CV_LOAD_IMAGE_COLOR);
 
   if (!image.data) {
     printf("No image data \n");
