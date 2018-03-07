171c93845c50acc9dcbcbf2eb165ad9dccf1bd2f
Jens Lindahl <jensctl@gmail.com>
2018-03-07 14:52:06 +0100

    Prepare CellSorter development

--- src/CellSorter/main.cpp
+++ src/CellSorter/main.cpp
@@ -1,30 +1,17 @@
 #include <iostream>
 
+#include "../FrameFinder/timer.h"
+#include "../FrameFinder/FrameFinder.h"
 #include "rbc.h"
-#include <opencv2/opencv.hpp>
+#include <opencv/cv.hpp>
 #include <stdio.h>
 
 using namespace cv;
+using namespace std;
 
 int main(int argc, char **argv) {
-  if (argc != 2) {
-    printf("usage: DisplayImage.out <Image_Path>\n");
-    return -1;
-  }
 
-  RBC a;
+    cout << "Hello world." << endl;
 
-  Mat image;
-  image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
-
-  if (!image.data) {
-    printf("No image data \n");
-    return -1;
-  }
-  namedWindow("Display Image", WINDOW_AUTOSIZE);
-  imshow("Display Image", image);
-
-  waitKey(0);
-
-  return 0;
+    return 0;
 }
