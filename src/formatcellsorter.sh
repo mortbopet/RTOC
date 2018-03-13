af3cde8d5e115787a36f19b2923772c1fb533985
Jens Lindahl <jensctl@gmail.com>
2018-03-13 21:25:46 +0100

    Add shell-script to only format files in cellsorter

---
+++ src/formatcellsorter.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# cd into cellsorter/
+cd cellsorter/
+# Grep all .cpp and .h files, and apply clang-format to themx
+for file in `git ls-files | grep ".cpp\|.h"`;
+do
+    clang-format -i -style=file $file
+done
