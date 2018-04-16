f442ec266a7e6d87f5b4f1a52a6a08103f584c30
Morten (lab computer) <morten_bp@live.dk>
2018-04-16 16:22:27 +0200

    Fix windows compilation

--- src/CellSorter/lib/matlab_ext.h
+++ src/CellSorter/lib/matlab_ext.h
@@ -1,11 +1,12 @@
 #ifndef CELLSORTER_MATLAB_EXT_H
 #define CELLSORTER_MATLAB_EXT_H
 
-#include <opencv/cv.hpp>
 #include <iterator>
-#include <cmath>
-#include <iostream>
+#include <opencv/cv.hpp>
+#define _USE_MATH_DEFINES
+#include <math.h>
 #include <algorithm>
+#include <iostream>
 
 #include "datacontainer.h"
 
@@ -38,20 +39,20 @@ void floodFill(cv::Mat& img, const cv::Point& p);
 double dist(const cv::Point& p0, const cv::Point& p1);
 
 template <typename T>
-std::pair<T,unsigned long> min(const std::vector<T>& v) {
-    std::pair<T,unsigned long> output;
+std::pair<T, unsigned long> min(const std::vector<T>& v) {
+    std::pair<T, unsigned long> output;
     auto m = std::min_element(std::begin(v), std::end(v));
     output.first = *m;
-    output.second = (unsigned) std::distance(std::begin(v), m);
+    output.second = (unsigned)std::distance(std::begin(v), m);
     return output;
 }
 
 template <typename T>
-std::pair<T,unsigned long> max(const std::vector<T>& v) {
-    std::pair<T,unsigned long> output;
+std::pair<T, unsigned long> max(const std::vector<T>& v) {
+    std::pair<T, unsigned long> output;
     auto m = std::max_element(std::begin(v), std::end(v));
     output.first = *m;
-    output.second = (unsigned) std::distance(std::begin(v), m);
+    output.second = (unsigned)std::distance(std::begin(v), m);
     return output;
 }
 
@@ -65,7 +66,6 @@ std::vector<T> find(std::vector<T>& v, T& term) {
     }
     return out;
 }
-
 
 }  // namespace matlab
 
