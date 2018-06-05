fc1f0a094066089f5b2654f34045ed68737b7a1a
Eskildbs <s163918@student.dtu.dk>
2018-06-05 10:08:53 +0200

    [machinelearning] - Transform class into basic polymorphic class

--- src/CellSorter/lib/machinelearning.cpp
+++ src/CellSorter/lib/machinelearning.cpp
@@ -1,19 +1,45 @@
 #include "machinelearning.h"
 
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string>
 
 Machinelearning::Machinelearning() {}
 
-void Machinelearning::loadModel(const std::string &path) {
+void Machinelearning::loadModel(const std::string& path) {}
+double Machinelearning::predictObject(DataContainer& dataContainer) {}
+
+int Machinelearning::findClosestXpos(const int& pos, DataContainer& dataContainer) {
+    for (int i = 0; i < _XBoundary; i++) {
+        int index = 0;
+        for (const auto& object : dataContainer) {
+            auto centroid = object->getValue<cv::Point>(data::Centroid);
+            if ((centroid.x > (pos - i)) && (centroid.x < (pos + i))) {
+                return index;
+            }
+            index++;
+        }
+    }
+}
+
+int Machinelearning::get_XBoundary() const {
+    return _XBoundary;
+}
+
+void Machinelearning::set_XBoundary(int m_XBoundary) {
+    Machinelearning::_XBoundary = m_XBoundary;
+}
+
+LogisticRegression::LogisticRegression() {}
+
+void LogisticRegression::loadModel(const std::string& path) {
     std::string line;
     std::ifstream file(path);
     int counter = 0;
-    while ( std::getline (file, line, ',') ) {
-        if (counter%3 == 0) {
+    while (std::getline(file, line, ',')) {
+        if (counter % 3 == 0) {
             xpos.push_back(std::stoi(line));
-        } else if (counter%3 == 1) {
+        } else if (counter % 3 == 1) {
             for (const auto& item : data::guiMap) {
                 if (item.second == line) {
                     attributes.push_back(std::get<1>(item.first));
@@ -26,20 +52,7 @@ void Machinelearning::loadModel(const std::string &path) {
     }
 }
 
-int Machinelearning::findClosestXpos(const int& pos, DataContainer& dataContainer) {
-    for (int i = 0; i < _XBoundary; i++) {
-        int index = 0;
-        for (const auto& object : dataContainer) {
-            auto centroid = object->getValue<cv::Point>(data::Centroid);
-            if (( centroid.x > (pos-i)) && (centroid.x < (pos+i) )) {
-                return index;
-            }
-            index++;
-        }
-    }
-}
-
-double Machinelearning::predictObject(DataContainer &dataContainer) {
+double LogisticRegression::predictObject(DataContainer& dataContainer) {
     double returnValue = 0;
     for (int i = 0; i < xpos.size(); i++) {
         // First, find the object with the given x-position
@@ -51,12 +64,4 @@ double Machinelearning::predictObject(DataContainer &dataContainer) {
         returnValue += (attributeValue * coefficients[i]);
     }
     return returnValue;
-}
-
-int Machinelearning::get_XBoundary() const {
-    return _XBoundary;
-}
-
-void Machinelearning::set_XBoundary(int m_XBoundary) {
-    Machinelearning::_XBoundary = m_XBoundary;
 }
--- src/CellSorter/lib/machinelearning.h
+++ src/CellSorter/lib/machinelearning.h
@@ -7,8 +7,8 @@
 class Machinelearning {
 public:
     Machinelearning();
-    void loadModel(const std::string& path);
-    double predictObject(DataContainer &dataContainer);
+    virtual void loadModel(const std::string& path);
+    virtual double predictObject(DataContainer &dataContainer);
 
     int get_XBoundary() const;
     void set_XBoundary(int m_XBoundary);
@@ -16,11 +16,20 @@ public:
 private:
     int findClosestXpos(const int& pos, DataContainer& dataContainer);
 
+    int _XBoundary = 250;
+
+};
+
+class LogisticRegression : public Machinelearning {
+public:
+    LogisticRegression();
+    void loadModel(const std::string& path) override;
+    double predictObject(DataContainer &dataContainer) override;
+
+private:
     std::vector<int> xpos;
     std::vector<data::DataFlags> attributes;
     std::vector<double> coefficients;
-
-    int _XBoundary = 250;
 
 };
 
