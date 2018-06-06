24100a6cf2849695a57eaae2f5df1b48dcdf8245
Eskildbs <s163918@student.dtu.dk>
2018-06-06 12:50:28 +0200

    [machinelearning] - update of machinelearning class.      - To use class:        -> call identifyModel to create object        -> call loadModel to load model        -> aftwards use predictObject to predict each datacontainer

--- src/CellSorter/lib/machinelearning.cpp
+++ src/CellSorter/lib/machinelearning.cpp
@@ -4,10 +4,31 @@
 #include <iostream>
 #include <string>
 
+std::unique_ptr<Machinelearning> identifyModel(const std::string& path) {
+    std::string line;
+    std::ifstream file(path);
+    while (std::getline(file, line, ',')) {
+        if (line == "LR") {
+            Machinelearning *model = new LogisticRegression();
+            auto ptr = std::unique_ptr<Machinelearning>{model};
+            return ptr;
+        }
+    }
+}
+
 Machinelearning::Machinelearning() {}
 
 void Machinelearning::loadModel(const std::string& path) {}
-double Machinelearning::predictObject(DataContainer& dataContainer) {}
+
+double Machinelearning::predictObject(DataContainer& dataContainer, double decisionBoundary) {}
+
+int Machinelearning::get_XBoundary() const {
+    return _XBoundary;
+}
+
+void Machinelearning::set_XBoundary(int m_XBoundary) {
+    Machinelearning::_XBoundary = m_XBoundary;
+}
 
 int Machinelearning::findClosestXpos(const int& pos, DataContainer& dataContainer) {
     for (int i = 0; i < _XBoundary; i++) {
@@ -22,22 +43,21 @@ int Machinelearning::findClosestXpos(const int& pos, DataContainer& dataContaine
     }
 }
 
-int Machinelearning::get_XBoundary() const {
-    return _XBoundary;
-}
-
-void Machinelearning::set_XBoundary(int m_XBoundary) {
-    Machinelearning::_XBoundary = m_XBoundary;
-}
-
 LogisticRegression::LogisticRegression() {}
 
 void LogisticRegression::loadModel(const std::string& path) {
     std::string line;
     std::ifstream file(path);
     int counter = 0;
+    int flag = 0;
     while (std::getline(file, line, ',')) {
-        if (counter % 3 == 0) {
+        if (flag == 0) {
+            if (counter == 1) {
+                intercept = std::stoi(line);
+                counter = -1;
+                flag = 1;
+            }
+        } else if (counter % 3 == 0) {
             xpos.push_back(std::stoi(line));
         } else if (counter % 3 == 1) {
             for (const auto& item : data::guiMap) {
@@ -52,8 +72,11 @@ void LogisticRegression::loadModel(const std::string& path) {
     }
 }
 
-double LogisticRegression::predictObject(DataContainer& dataContainer) {
-    double returnValue = 0;
+double LogisticRegression::predictObject(DataContainer& dataContainer, double decisionBoundary) {
+    // customParameter defines the decision boundary between the binary classification.
+    // By default this should be set to 0.5.
+
+    double possibility = 0;
     for (int i = 0; i < xpos.size(); i++) {
         // First, find the object with the given x-position
         int index = findClosestXpos(xpos[i], dataContainer);
@@ -61,7 +84,28 @@ double LogisticRegression::predictObject(DataContainer& dataContainer) {
         // Next, extract the right attribute and multiply its coefficient onto the model
         // Make arguement change with attribute-type
         auto attributeValue = dataContainer[index]->getValue<double>(attributes[i]);
-        returnValue += (attributeValue * coefficients[i]);
+        possibility += (attributeValue * coefficients[i]);
     }
-    return returnValue;
+    // Add intercept
+    possibility = exp(possibility + intercept);
+
+    // Compute possibility
+    possibility = possibility / (1 + possibility);
+
+    // Checks if possibility is above the chosen threshold
+    if (possibility >= decisionBoundary) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+
+ArtificialNeuralNetwork::ArtificialNeuralNetwork() {
+}
+
+DecisionTree::DecisionTree() {
+}
+
+NaiveBayes::NaiveBayes() {
 }
--- src/CellSorter/lib/machinelearning.h
+++ src/CellSorter/lib/machinelearning.h
@@ -8,29 +8,55 @@ class Machinelearning {
 public:
     Machinelearning();
     virtual void loadModel(const std::string& path);
-    virtual double predictObject(DataContainer &dataContainer);
+
+    virtual double predictObject(DataContainer& dataContainer, double decisionBoundary=0.5);
 
     int get_XBoundary() const;
     void set_XBoundary(int m_XBoundary);
 
-private:
+protected:
+    std::vector<int> xpos;
+    std::vector<data::DataFlags> attributes;
     int findClosestXpos(const int& pos, DataContainer& dataContainer);
 
+private:
     int _XBoundary = 250;
-
 };
 
 class LogisticRegression : public Machinelearning {
 public:
     LogisticRegression();
     void loadModel(const std::string& path) override;
-    double predictObject(DataContainer &dataContainer) override;
+
+    double predictObject(DataContainer& dataContainer, double decisionBoundary) override;
 
 private:
-    std::vector<int> xpos;
-    std::vector<data::DataFlags> attributes;
     std::vector<double> coefficients;
-
+    int intercept;
 };
 
-#endif //CELLSORTER_MACHINELEARNING_H
+class ArtificialNeuralNetwork : public Machinelearning {
+public:
+    ArtificialNeuralNetwork();
+
+private:
+    std::vector<int> weights;
+};
+
+class DecisionTree : public Machinelearning {
+public:
+    DecisionTree();
+
+private:
+};
+
+class NaiveBayes : public Machinelearning {
+public:
+    NaiveBayes();
+
+private:
+};
+
+std::unique_ptr<Machinelearning> identifyModel(const std::string& path);
+
+#endif  // CELLSORTER_MACHINELEARNING_H
