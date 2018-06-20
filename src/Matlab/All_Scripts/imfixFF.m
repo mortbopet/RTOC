6d273cbe3ba6d6019c28299f4ebdbbff051b3448
Jens Lindahl <jensctl@gmail.com>
2018-06-20 09:47:44 +0200

    Remove matlab scripts

--- src/Matlab/All_Scripts/imfixFF.m
+++
@@ -1,9 +0,0 @@
- function img=imfixFF(img,img2)
-% CONVERT TO GRAYSCALE DOUBLES
-img = double(rgb2gray(img));
-img2 = double(rgb2gray(img2));
-% NORMALIZE
-img = img / 255;
-img2 = img2 / 255;
-% SUBTRACT
-img = img-img2;
